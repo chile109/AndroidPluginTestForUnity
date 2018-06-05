dcaad2002224eb9d94b53ae46015d7e3aec48f8d
Kevin Chang <kevin@megalive.com.tw>
2018-06-05 16:16:45 +0800

    拍照 unity 接口完成

---
+++ Assets/Scripts/PhotoPlugin.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhotoPlugin : MonoBehaviour {
+
+	AndroidJavaClass unityPlayer;
+	AndroidJavaObject currentActivity;
+
+	AndroidJavaObject _ajo;
+	Texture texture;
+
+	public RawImage Pic;
+	public Button _CameraBtn;
+	public Button _GalleyBtn;
+
+	// Use this for initialization
+	void Start () {
+
+		unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+		currentActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+
+		_ajo = new AndroidJavaObject("com.kevin.unity.NojarAndroid");
+		_ajo.CallStatic("init", currentActivity);
+
+		_CameraBtn.onClick.AddListener(delegate
+		{
+			PhotoGalley("takePhoto");
+		});
+
+		_GalleyBtn.onClick.AddListener(delegate
+		{
+			PhotoGalley("openGalley");
+		});
+	}
+	
+	void PhotoGalley(string target)
+	{
+		_ajo.Call("PhotoMethod", target);
+	}
+
+	void getTexture(string str)
+	{
+		//在Android插件中通知Unity开始去指定路径中找图片资源
+		StartCoroutine(LoadTexture(str));
+
+	}
+
+	IEnumerator LoadTexture(string ImgName)
+	{
+		//注解1
+		string path = "file://" + Application.persistentDataPath + "/" + ImgName;
+
+		WWW www = new WWW(path);
+		while (!www.isDone)
+		{
+
+		}
+		yield return www;
+		//为贴图赋值
+		texture = www.texture;
+
+		Pic.texture = texture;
+	}
+}
